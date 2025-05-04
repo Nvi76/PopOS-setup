2328af84c5f468ab3425ea878870573e83014e55
Chorusn <nvme12@proton.me>
2025-05-04 14:45:58 +0700

    Update de.sh

--- de.sh
+++ de.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-chmod +x secure.sh && chmod +x ollama.sh && chmod +x updater.sh && chmod +x apps.sh && chmod +x cliapps.sh && sudo apt update -y && sudo apt upgrade -y && sudo snap refresh && sudo snap install firefox thunderbird snap-center && sudo apt install git curl nala -y && sudo nala install ubuntu-gnome-desktop ubuntu-desktop plymouth plymouth-themes* network-manager -y && sudo systemctl start NetworkManager.service && sudo systemctl enable NetworkManager.service && sudo update-alternatives --config default.plymouth && sudo nano /etc/gdm3/custom.conf && sudo nano /etc/default/grub && sudo update-grub && 
+chmod +x secure.sh && chmod +x ollama.sh && chmod +x updater.sh && chmod +x apps.sh && chmod +x cliapps.sh && sudo apt update -y && sudo apt upgrade -y && sudo snap refresh && sudo snap install firefox thunderbird snap-center && sudo apt install git curl nala -y && sudo nala install ubuntu-gnome-desktop ubuntu-desktop plymouth plymouth-themes* network-manager -y && sudo systemctl start NetworkManager.service && sudo systemctl enable NetworkManager.service && sudo systemctl disable systemd-networkd-wait-online.service && sudo systemctl disable NetworkManager-wait-online.service && sudo update-alternatives --config default.plymouth && sudo nano /etc/gdm3/custom.conf && sudo nano /etc/default/grub && sudo update-grub && 
 sudo update-initramfs -u
-
-
