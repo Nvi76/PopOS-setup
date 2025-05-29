ebdbb570faf08da09e387e17e70466c55bea1ac2
chorusn <nvme12@proton.me>
2025-05-29 14:33:51 +0700

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 
-sudo apt update && sudo apt upgrade && sudo snap refresh && flatpak update && atuin-update && brew update && brew upgrade && curl -o /tmp/hblock 'https://raw.githubusercontent.com/hectorm/hblock/v3.5.1/hblock' \
+set -euo pipefail
+
+# Updates the system
+sudo apt update -y && sudo apt upgrade -y
+
+# Updating snap apps
+sudo snap refresh 
+
+# Updating flatpak apps
+flatpak update 
+
+# Updating Atuin
+atuin-update 
+
+# Updating Homebrew
+brew update && brew upgrade
+
+# Updating hblock
+curl -o /tmp/hblock 'https://raw.githubusercontent.com/hectorm/hblock/v3.5.1/hblock' \
   && echo 'd010cb9e0f3c644e9df3bfb387f42f7dbbffbbd481fb50c32683bbe71f994451  /tmp/hblock' | shasum -c \
   && sudo mv /tmp/hblock /usr/local/bin/hblock \
   && sudo chown 0:0 /usr/local/bin/hblock \
-  && sudo chmod 755 /usr/local/bin/hblock && hblock && sudo freshclam
+  && sudo chmod 755 /usr/local/bin/hblock && hblock 
+
+# Updating clamav
+sudo freshclam
