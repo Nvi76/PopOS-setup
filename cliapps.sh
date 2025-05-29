f8e4a8704b2a060e6e95b78bd415235bd01ba193
chorusn <nvme12@proton.me>
2025-05-29 14:18:54 +0700

    Update cliapps.sh

--- cliapps.sh
+++ cliapps.sh
@@ -1,3 +1,19 @@
 #!/bin/bash 
 
-sudo nala install build-essential nautilus-extension-gnome-terminal -y && curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh && /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" && sudo apt-get install build-essential -y
+set -euo pipefail
+
+# Installing essential packages
+sudo nala install build-essential nautilus-extension-gnome-terminal -y
+
+# Installing Atuin
+curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh
+
+# Installing Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" 
+
+# Following Homebrews' instructions
+sudo apt-get install build-essential -y
+
+# Following Homebrews' instructions
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.profile
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
