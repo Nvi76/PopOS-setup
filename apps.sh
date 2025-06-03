a6c4335f3502376106dfbc695437b8dbd94c4d51
chorusn <nvme12@proton.me>
2025-06-03 12:41:15 +0700

    Update apps.sh

--- apps.sh
+++ apps.sh
@@ -6,14 +6,14 @@ set -euo pipefail
 sudo apt update -y && sudo apt upgrade -y
 
 # Installing snap applications
-sudo snap refresh && sudo snap install code --classic && sudo snap install codium --classic && sudo snap install qbittorrent-arnatious notion-desktop element-desktop varia discord vlc telegram-desktop && sudo snap connect varia:shutdown 
+sudo snap refresh && sudo snap install code --classic && sudo snap install codium --classic && sudo snap install firmware-updater qbittorrent-arnatious notion-desktop element-desktop varia discord vlc telegram-desktop && sudo snap connect varia:shutdown 
 
 # Importing signal's keyring and repo
 wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg &&
 cat signal-desktop-keyring.gpg | sudo tee /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null && echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' |\ sudo tee /etc/apt/sources.list.d/signal-xenial.list
 
-# Downloading safing portmaster and anytype
-curl https://updates.safing.io/latest/linux_amd64/packages/portmaster-installer.deb --output portmaster.deb && curl https://anytype-release.fra1.cdn.digitaloceanspaces.com/anytype_0.46.8_amd64.deb --output anytype.deb
+# Downloading anytype
+curl https://anytype-release.fra1.cdn.digitaloceanspaces.com/anytype_0.46.8_amd64.deb --output anytype.deb
 
 # Importing brave browser's repo
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg && echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list 
@@ -28,7 +28,7 @@ sudo nala install flatpak neovim kitty kakoune nautilus-extension-gnome-terminal
 figlet 40% Complete
 
 # Installing locally Downloaded deb packages
-sudo nala install ./portmaster.deb -y && sudo nala install ./anytype.deb
+sudo nala install ./anytype.deb
 
 # Importing flathub's repo
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
