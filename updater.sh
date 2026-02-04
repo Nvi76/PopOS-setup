f7b37e0a7e19663115a27c1984da2563137bcbef
VistaBlue <nvme12@proton.me>
2026-02-04 20:24:11 +0700

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -1,25 +1,21 @@
-#!/bin/bash
+#!/usr/bin/env fish
 
-set -euo pipefail
+# Update system
+sudo nala update -y
+and sudo nala upgrade -y
 
-# Updates the system
-sudo apt update -y && sudo apt upgrade -y
+# Update Flatpak apps
+flatpak update -y
 
-# Updating flatpak apps
-flatpak update 
+# Update Homebrew
+brew update
+and brew upgrade
 
-# Updating Atuin
-atuin-update 
+# Update hblock
+hblock
 
-# Updating Homebrew
-brew update && brew upgrade
-
-# Updating hblock
-curl -o /tmp/hblock 'https://raw.githubusercontent.com/hectorm/hblock/v3.5.1/hblock' \
-  && echo 'd010cb9e0f3c644e9df3bfb387f42f7dbbffbbd481fb50c32683bbe71f994451  /tmp/hblock' | shasum -c \
-  && sudo mv /tmp/hblock /usr/local/bin/hblock \
-  && sudo chown 0:0 /usr/local/bin/hblock \
-  && sudo chmod 755 /usr/local/bin/hblock && hblock 
-
-# Updating clamav
+# Update ClamAV
 sudo freshclam
+
+# Update complete
+figlet Update Complete
