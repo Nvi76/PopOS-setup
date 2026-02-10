69efa44d4e6feef4d830919e16bce110b24dcad7
VistaBlue <nvme12@proton.me>
2026-02-11 05:13:53 +0700

    Update cliapps.fish

--- cliapps.fish
+++ cliapps.fish
@@ -5,7 +5,7 @@ sudo nala update; or exit 1
 sudo nala upgrade -y; or exit 1
 
 # Install Cliapps
-sudo nala install -y figlet fish curl git vulkan-tools os-prober build-essential python3-tk python3-distutils-extras pipx; or exit 1
+sudo nala install -y figlet fish curl git vulkan-tools os-prober build-essential python3-tk python3-distutils-extra pipx; or exit 1
 
 # Install Atuin
 curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh; or exit 1
