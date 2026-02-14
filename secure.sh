8121eb508783776274c6c8711cdbbee570dada23
VistaBlue <nvme12@proton.me>
2026-02-14 12:57:16 +0700

    Update secure.sh

--- secure.sh
+++ secure.sh
@@ -22,11 +22,21 @@ sudo apt update
 sudo apt install -y build-essential nala fish curl git
 
 # Install security apps
-sudo nala install -y clamav
+sudo nala install -y clamav clamav-base clamav-daemon clamav-freshclam
 sudo nala install -y ./portmaster.deb || sudo apt -f install -y
 
 # Cleanup
 rm -f portmaster.deb
+
+# Enabling Services
+sudo systemctl enable --now clamav-freshclam
+sudo systemctl enable --now clamav-daemon
+sudo systemctl enable --now clamav-clamonacc  
+
+# Configuring the clamd.conf file
+sudo nano /etc/clamav/clamd.conf
+# OnAccessIncludePath /home
+# OnAccessExcludePath /home/user/Downloads   
 
 # Update ClamAV
 sudo systemctl stop clamav-freshclam
@@ -34,6 +44,6 @@ sudo freshclam
 sudo systemctl start clamav-freshclam
 
 # Set default shell
-chsh -s "$(command -v fish)"
+chsh -s /usr/bin/fish
 
 figlet "Setup Complete Log out and Log in to use fish" 2>/dev/null; or echo "Setup Complete Log out and Log in to use fish"
