2ac409acb8a1aca9a7af807f92321ca993b0e788
VistaBlue <nvme12@proton.me>
2026-02-15 08:17:49 +0700

    Update apps.fish

--- apps.fish
+++ apps.fish
@@ -59,7 +59,7 @@ sudo nala upgrade -y; or exit 1
 
 # Homebrew apps
 if type -q brew
-    brew install neovim thefuck fzf ranger btop trash-cli ffmpeg fastfetch
+    brew install neovim fzf ranger btop trash-cli ffmpeg fastfetch
 end
 
 # Installing nvidia drivers
