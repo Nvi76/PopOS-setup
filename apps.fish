b2b8b44e1e73f73b704a564aa9fb812eb103a388
VistaBlue <nvme12@proton.me>
2026-02-15 15:47:05 +0700

    Update apps.fish

--- apps.fish
+++ apps.fish
@@ -4,31 +4,6 @@
 sudo nala update; or exit 1
 sudo nala upgrade -y; or exit 1
 
-# Import Brave keyring
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg \
-https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg; or exit 1
-
-# Add Brave repo
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" \
-| sudo tee /etc/apt/sources.list.d/brave-browser-release.list >/dev/null; or exit 1
-
-# Add luanti's PPA
-sudo add-apt-repository ppa:minetestdevs/stable -y; or exit 1
-
-# Import VSCodium keyring
-wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg \
-| gpg --dearmor \
-| sudo tee /usr/share/keyrings/vscodium-archive-keyring.gpg >/dev/null; or exit 1
-
-# Add VSCodium repo
-printf "Types: deb
-URIs: https://download.vscodium.com/debs
-Suites: vscodium
-Components: main
-Architectures: amd64 arm64
-Signed-by: /usr/share/keyrings/vscodium-archive-keyring.gpg
-" | sudo tee /etc/apt/sources.list.d/vscodium.sources >/dev/null; or exit 1
-
 # Download VS Code
 curl -fL \
 https://vscode.download.prss.microsoft.com/dbazure/download/stable/bdd88df003631aaa0bcbe057cb0a940b80a476fa/code_1.109.0-1770171879_amd64.deb \
